4aba9c38f209e5b77eb1c657839e56bdbda57da8
violettadudiak0210 <violettadudiak0210@gmail.com>
2025-07-28 03:14:50 +0100

    Initial commit: working NoteHub app

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import App from "./components/App/App";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
+);
